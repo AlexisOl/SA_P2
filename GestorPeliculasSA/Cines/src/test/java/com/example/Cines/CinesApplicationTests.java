14e41f8e63338b17049127eaf3addeaae941da3a
Alexxus <oleoalexis@gmail.com>
2025-10-03 10:36:28 -0600

    ingreso de estructura general

---
+++ GestorPeliculasSA/Cines/src/test/java/com/example/Cines/CinesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Cines;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
