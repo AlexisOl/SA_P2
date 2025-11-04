7c92c3268d5a47963dd8069d001272c03d753fb2
Alexxus <oleoalexis@gmail.com>
2025-11-03 20:31:43 -0600

    cambio de value a data por el constructor de jackson

--- Cines/src/main/java/com/example/Cines/CostosCine/Aplicacion/CasoUso/CrearCostoCIne/CrearCostosCineDTO.java
+++ Cines/src/main/java/com/example/Cines/CostosCine/Aplicacion/CasoUso/CrearCostoCIne/CrearCostosCineDTO.java
@@ -1,12 +1,17 @@
 package com.example.Cines.CostosCine.Aplicacion.CasoUso.CrearCostoCIne;
 
 import com.example.Cines.Cine.Dominio.Cine;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.Value;
 
 import java.time.LocalDate;
 import java.util.UUID;
 
-@Value
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class CrearCostosCineDTO {
     private LocalDate fecha;
     private Double costo;
--- Cines/src/main/java/com/example/Cines/Salas/Aplicacion/CasoUso/CrearSala/CrearSalaDTO.java
+++ Cines/src/main/java/com/example/Cines/Salas/Aplicacion/CasoUso/CrearSala/CrearSalaDTO.java
@@ -4,11 +4,16 @@ import com.example.Cines.Cine.Dominio.Cine;
 import com.example.Cines.Salas.Dominio.ObjetosValor.MatrizAsientos;
 import com.example.Cines.Salas.Dominio.ObjetosValor.ValidacionesSala;
 import com.example.Cines.Salas.Dominio.TipoSala;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.Value;
 
 import java.util.UUID;
 
-@Value
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class CrearSalaDTO {
 
     private String nombre;
